c548c642ab8bbe017c5b6e05fb60ec875d66c850
ToBeReplaced <ToBeReplaced@gmail.com>
2013-04-19 20:25:26 -0400

    clojure 1.4 profile

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/ToBeReplaced/jdbc-format"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
